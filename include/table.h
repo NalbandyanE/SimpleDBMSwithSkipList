4fbe85323c9d04d6231ddc3a5cd822954ce2db99
Emi <89019222+NalbandyanE@users.noreply.github.com>
2023-08-09 22:48:43 +0400

    Update table.h

--- include/table.h
+++ include/table.h
@@ -2,7 +2,6 @@
 #define TABLE_H
 
 #include "row.h"
-// #include "index.h"
 #include "skiplist.h"
 
 #include <vector>
@@ -13,7 +12,6 @@ class Table {
 public:
 	Table() = default;
 	Table(const std::string&);
-	// Table(const std::string&, const std::vector<std::string>&, int);
 	void insertRow(const Row&);
 	void updateRow(int, const Row&);
 	std::vector<Row> selectAll() const;
@@ -25,19 +23,14 @@ public:
 	void show() const;
 	bool loadFromFile(const std::string&, Row&) const;
 	bool loadFromFile(const std::string&);
-    bool saveToFile(const std::string&, const Row&) const;
-    bool meetCondition(const Row&, const std::string&);
-    // bool hasIndexOn(const std::string&) const;
-    // Index& getIndex(const std::string&);
-    void createIndex(const std::string&, int);
+    	bool saveToFile(const std::string&, const Row&) const;
+    	bool meetCondition(const Row&, const std::string&);
+    	void createIndex(const std::string&, int);
 private:
 	std::vector<Row> rows;
 	std::string name;
 	std::vector<std::string> rowFiles;
 	std::unordered_map<std::string, std::shared_ptr<SkipList>> indexes;
-	// std::unordered_map<std::string, Index*> indexes;
-	// int order;
-	// std::vector<std::string> columns;
 	std::string generateFileName(int) const;
 };
 
